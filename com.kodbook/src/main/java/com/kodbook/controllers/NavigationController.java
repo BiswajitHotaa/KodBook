335eb43125d26cb01c0fc44c7d0282ba6bc500e7
BiswajitHotaa <biswajithotaa@gmail.com>
2024-09-23 12:35:21 +0530

    my first commit

---
+++ com.kodbook/src/main/java/com/kodbook/controllers/NavigationController.java
@@ -0,0 +1,28 @@
+package com.kodbook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavigationController 
+{
+
+	@GetMapping("/")
+	public String indexPage()
+	{
+		return "index";
+	}
+
+	@GetMapping("/openSignUp")
+	public String openSignUpPage()
+	{
+		return "signUp";
+	}
+	
+	@GetMapping("/openCreatePost")
+	public String openCreatePost()
+	{
+		return "createPost";
+	}
+
+}
---
+++ com.kodbook/src/main/java/com/kodbook/controllers/PostController.java
@@ -0,0 +1,51 @@
+package com.kodbook.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kodbook.entities.Post;
+import com.kodbook.services.PostService;
+
+@Controller
+public class PostController 
+{
+	@Autowired
+	PostService service;
+	
+	
+	@PostMapping("/createPost")
+	public String createPost(@RequestParam("caption") String caption,
+							@RequestParam("photo") MultipartFile photo
+							)
+	{
+		Post post=new Post();
+		post.setCaption(caption);
+		
+		try {
+			
+			post.setPhoto(photo.getBytes());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		service.createPost(post);
+		return "home";
+	}
+	
+	@GetMapping("/showPosts")
+	public String showPosts(Model model)
+	{
+		List<Post> allPosts=service.fetchAllPosts();
+		model.addAttribute("allPosts", allPosts);
+		return "showPosts";
+	}
+
+}
